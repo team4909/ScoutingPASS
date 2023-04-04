a1425040b577d4c564c776389e2f43d88a68a3c3
LTeixeira4909 <119272952+LTeixeira4909@users.noreply.github.com>
2023-04-04 18:48:57 -0400

    Revert "update api"
    This reverts commit b2ad79dfc9502267e2b7ee1ae50dd7c194fe025f.

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ let CU_data = {
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023necmp2",
+      "defaultValue": "2023necmp",
       "required": "true",
       "disabled": "true"
     },
